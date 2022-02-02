9a09e745ee295d39fe57023209300f5459846fd0
Programisto1011 <victormaynougomez@gmail.com>
2022-02-02 16:32:06 +0000

    Can Stop

--- src/js/view/Main.jsx
+++ src/js/view/Main.jsx
@@ -6,20 +6,30 @@ const Main = () => {
 	const [seconds, setSeconds] = useState(0);
 
 	useEffect(() => {
+		//Se ejecuta la función cada segundo y se suma uno a seconds
 		const interval = setInterval(() => {
 			setSeconds((seconds) => seconds + 1);
 		}, 1000);
-		return () => clearInterval(interval);
+		//Se muestra un "alert" a los 5 segundos y se para de ejecutar la función
+		setTimeout(() => {
+			clearInterval(interval);
+			alert("stop");
+		}, 55000);
 	}, []);
+
+	const seconds10 = Math.floor({ seconds } / 10);
+	const seconds100 = Math.floor({ seconds } / 100);
+	const seconds1000 = Math.floor({ seconds } / 1000);
+
+	let items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+
 	return (
 		<div>
 			<div className="row">
+				<Card number={seconds1000} />
+				<Card number={seconds100} />
+				<Card number={seconds10} />
 				<Card number={seconds} />
-				<Card number="1" />
-				<Card number="1" />
-				<Card number="1" />
-				<Card number="1" />
-				<Card number="1" />
 			</div>
 		</div>
 	);
