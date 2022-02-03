42e1d34c8523fb98c095a758d74fc3c83bc30911
Programisto1011 <victormaynougomez@gmail.com>
2022-02-03 11:10:12 +0000

    Alpha version

--- src/js/view/Main.jsx
+++ src/js/view/Main.jsx
@@ -1,34 +1,42 @@
 import React, { useState, useEffect } from "react";
+import "../../styles/Main.css";
 import Card from "../component/Card.jsx";
+//Install: npm install react-bootstrap bootstrap@5.1.3
 
-//create your first component
 const Main = () => {
-	const [seconds, setSeconds] = useState(0);
+	const [seconds, setSeconds] = useState(1);
 
 	useEffect(() => {
 		//Se ejecuta la función cada segundo y se suma uno a seconds
 		const interval = setInterval(() => {
 			setSeconds((seconds) => seconds + 1);
 		}, 1000);
-		//Se muestra un "alert" a los 5 segundos y se para de ejecutar la función
+		//Units transform seg. -> miliseg.
+		const mSecondStop = 9999 * 1000;
+		//Se muestra un "alert" a los 9999 segundos y se para de ejecutar la función
 		setTimeout(() => {
 			clearInterval(interval);
-			alert("stop");
-		}, 22000);
+			alert(
+				"Han pasado 9999 segundos - se ha llegado al límite del contador."
+			);
+		}, mSecondStop);
 	}, []);
 
-	const seconds1 = String(seconds % 10);
-	const seconds10 = String(Math.floor(seconds / 10));
-	const seconds100 = Math.floor(seconds / 100);
 	const seconds1000 = Math.floor(seconds / 1000);
+	const rSeconds1000 = seconds % 1000;
+	const seconds100 = Math.floor(rSeconds1000 / 100);
+	const rSeconds100 = rSeconds1000 % 100;
+	const seconds10 = String(Math.floor(rSeconds100 / 10));
+	const rSeconds10 = rSeconds100 % 10;
 
 	return (
-		<div>
-			<div className="row">
+		<div class="container-fluid bg-light align-items-center mt-5 ">
+			<div className="row text-center">
+				<div class="col-2" />
 				<Card number={seconds1000} />
 				<Card number={seconds100} />
 				<Card number={seconds10} />
-				<Card number={seconds1} />
+				<Card number={rSeconds10} />
 			</div>
 		</div>
 	);
